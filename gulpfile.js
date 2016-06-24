5d95b578185f4fdc08a502ef8de64297a7ebd6f3
Chris Bergstrom <chris@snapcare.io>
2016-06-23 17:14:32 -0700

    Split out "build" and "serve" steps for heroku.

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var webpack = require('webpack-stream');
 var jsonServer = require('json-server');
 
 // Webpack
-gulp.task('webpack', function() {
+gulp.task('build', function() {
   return gulp.src('./src/app.js')
     .pipe(webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('./public'));
 });
 
 // JSON API Server - run a REST server via a simple json file
-gulp.task('serve:api', function(cb) {
+gulp.task('serve', function(cb) {
   var apiServer = jsonServer.create();
   var router = jsonServer.router('db.json');
 
@@ -23,9 +23,7 @@ gulp.task('serve:api', function(cb) {
 
 // Watch for changes and reload stuff
 gulp.task('watch', function() {
-  gulp.watch('./src/**/*', ['webpack']);
+  gulp.watch('./src/**/*', ['build']);
 });
 
-gulp.task('run', ['webpack', 'serve:api']);
-
-gulp.task('default', ['webpack', 'serve:api', 'watch']);
+gulp.task('default', ['build', 'serve', 'watch']);
